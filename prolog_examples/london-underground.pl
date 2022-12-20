e97e65d2faf18075f550b9758e12a77c070e996c
Samuel Hicks <SamMHicks@hotmail.co.uk>
2022-12-20 20:36:11 +0000

    Improved London Underground example

--- prolog_examples/london-underground.pl
+++ prolog_examples/london-underground.pl
@@ -1,24 +1,20 @@
-
-
-connected(bond_street, oxford_circus, central).
-connected(oxford_circus, tottenham_court_road, central).
-connected(bond_street, green_park, jubilee).
-connected(green_park, charing_cross, jubilee).
-connected(green_park, piccadilly_circus, piccadilly).
-connected(piccadilly_circus, leicester_square, piccadilly).
-connected(green_park, oxford_circus, victoria).
-connected(oxford_circus, piccadilly_circus, bakerloo).
-connected(piccadilly_circus, charing_cross, bakerloo).
-connected(tottenham_court_road, leicester_square, northern).
-connected(leicester_square, charing_cross, northern).
-
-
-on_line(Station, Line) :-
-	connected(Station, _, Line).
-
-on_line(Station, Line) :-
-	connected(_, Station, Line).
-
+on_line(bond_street, central).
+on_line(bond_street, jubilee).
+on_line(charing_cross, bakerloo).
+on_line(charing_cross, jubilee).
+on_line(charing_cross, northern).
+on_line(green_park, jubilee).
+on_line(green_park, piccadilly).
+on_line(green_park, victoria).
+on_line(leicester_square, northern).
+on_line(leicester_square, piccadilly).
+on_line(oxford_circus, bakerloo).
+on_line(oxford_circus, central).
+on_line(oxford_circus, victoria).
+on_line(piccadilly_circus, bakerloo).
+on_line(piccadilly_circus, piccadilly).
+on_line(tottenham_court_road, central).
+on_line(tottenham_court_road, northern).
 
 same_line(Station1, Station2, Line) :-
 	on_line(Station1, Line),
@@ -31,22 +27,22 @@ journey_to(From, To, Route) :-
 
 journey_to(From, To, [(Line:(From-To))], Already_Visited) :-
 	same_line(From, To, Line),
-	not_member(line(Line), Already_Visited),
-	not_member(station(To), Already_Visited).
+	not_member(Already_Visited, line(Line)),
+	not_member(Already_Visited, station(To)).
 
 journey_to(From, To, [(Line:(From-Intermediate))|Route], Already_Visited) :-
 	same_line(From, Intermediate, Line),
-	not_member(station(Intermediate), Already_Visited),
-	not_member(line(Line), Already_Visited),
+	not_member(Already_Visited, station(Intermediate)),
+	not_member(Already_Visited, line(Line)),
 	journey_to(Intermediate, To, Route, [station(Intermediate),line(Line)|Already_Visited]).
 
 
-not_member(_Item, []) :-
+not_member([], _Item) :-
 	!.
 
-not_member(Item, [Item|_]) :-
+not_member([Item|_], Item) :-
 	!,
 	fail.
 
-not_member(Item, [_|Items]) :-
-	not_member(Item, Items).
+not_member([_|Items], Item) :-
+	not_member(Items, Item).
